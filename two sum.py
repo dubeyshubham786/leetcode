47ca805df51a86c06c824bf536b6a5b518cb056b
Shubham Dubey <dubeyshubham154@gmail.com>
2022-04-01 12:37:56 +0530

    Add files via upload

---
+++ two sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        d = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in d:
+                return [d[diff], i]
+            else:
+                d[n] = i
